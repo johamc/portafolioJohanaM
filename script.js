e0261f520a38c28b90f6f83fe880196d03b96c37
johamc <joha-monsalve@hotmail.com>
2026-01-05 14:48:17 -0500

    portafolio

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById('themeToggle');
+const body = document.body;
+
+toggle.addEventListener('click', () => {
+  body.classList.toggle('dark');
+  toggle.textContent = body.classList.contains('dark') ? '☀️' : '🌙';
+});
