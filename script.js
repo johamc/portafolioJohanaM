c79dc312d26ece25204271aaee4713f128abb9a9
johamc <joha-monsalve@hotmail.com>
2026-01-05 21:46:03 -0500

    skills

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ toggle.addEventListener('click', () => {
   body.classList.toggle('dark');
   toggle.textContent = body.classList.contains('dark') ? '☀️' : '🌙';
 });
+
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 120;
+      if (scrollY >= sectionTop) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === `#${current}`) {
+        link.classList.add("active");
+      }
+    });
+  });
+
